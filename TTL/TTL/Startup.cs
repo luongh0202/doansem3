3e0a90286a2ca45934e7c1a10e69453434b31a29
LuongHL <luongh0202@gmail.com>
2019-08-17 09:10:48 +0700

    upload pr

---
+++ TTL/TTL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TTL.Startup))]
+namespace TTL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
