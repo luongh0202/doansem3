4bc6208fb57557dd94dad433c5797ec21cca3205
tien <nmr.tien@gmail.com>
2019-09-09 20:51:04 +0700

    update bt Tien

---
+++ LTT/LTT/Areas/Backend/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTT.Areas.Backend.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Backend/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
