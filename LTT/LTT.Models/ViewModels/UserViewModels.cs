f0ee77b5e931c7cada833b57b1c2c6155264d66a
LuongHL <luongh0202@gmail.com>
2019-09-20 20:38:43 +0700

    upload

---
+++ LTT/LTT.Models/ViewModels/UserViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTT.Models.ViewModels
+{
+    public class UserRegister
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "text")]
+        public string UserAddress { get; set; }
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class UserLogin
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
